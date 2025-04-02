fb98c80c2103ac1308d7e30d262bf721df7ee1ce
SpaceTimeWarrior <stevenjr@comporium.net>
2025-04-01 21:29:21 -0400

    start 1.20.6+ mod development

--- src/main/java/net/tsw/temporallight/block/custom/customBlock.java
+++
@@ -1,9 +0,0 @@
-package net.tsw.temporallight.block.custom;
-
-import net.minecraft.block.Block;
-
-public class customBlock extends Block {
-    public customBlock(Properties properties) {
-        super(properties);
-    }
-}
